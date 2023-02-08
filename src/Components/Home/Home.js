78a3a83ed0065c1483832b10979ce5762d4b9a50
Trezie <ifytrez96@gmail.com>
2023-02-08 23:27:38 +0100

    my second commit

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -94,6 +94,19 @@ function Home() {
             </div>
 
         </section>
+        <section className='large'>
+            <div className='largeBox'>
+                <div className='firstRec'>
+                    <img src="" />
+                    <div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            
+        </section>
         <div className='rightSecondSection'>
                     <h1>About Femcode Africa</h1>
                     <span className='' style={{marginTop: '10px'}}>FemCode Africa is a virtual tech education company focused on <br/> training young females in Africa and helping them get employed. </span>
